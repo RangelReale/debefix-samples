82cb0d7bebad26c10d7965729eeb5c192aef4a8b
Rangel Reale <rangelspam@gmail.com>
2024-06-17 17:14:48 -0300

    string

--- file/copyfile/string_test.go
+++ file/copyfile/string_test.go
@@ -52,6 +52,21 @@ func TestParse(t *testing.T) {
 				"company} sample {nonna": "{company}} sample {nonna}",
 			},
 		},
+		{
+			name: "escaped repeated",
+			str:  "test {{{company} sample {nonna}",
+			expectedFields: map[string]string{
+				"company": "",
+				"nonna":   "",
+			},
+		},
+		{
+			name: "escaped repeated 4",
+			str:  "test {{{{company} sample {nonna}",
+			expectedFields: map[string]string{
+				"nonna": "",
+			},
+		},
 	} {
 		t.Run(test.name, func(t *testing.T) {
 			p := Parse(test.str)
@@ -63,6 +78,61 @@ func TestParse(t *testing.T) {
 					fexpected = fmt.Sprintf("{%s}", field.name)
 				}
 				assert.Equal(t, fexpected, fv)
+			}
+		})
+	}
+}
+
+func TestReplace(t *testing.T) {
+	for _, test := range []struct {
+		name        string
+		str         string
+		values      map[string]any
+		expected    string
+		expectedErr bool
+	}{
+		{
+			name: "simple",
+			str:  "test {company} sample {nonna}",
+			values: map[string]any{
+				"company": "666",
+				"nonna":   "888",
+			},
+			expected: "test 666 sample 888",
+		},
+		{
+			name: "missing",
+			str:  "test {company} sample {nonna}",
+			values: map[string]any{
+				"company": "666",
+			},
+			expectedErr: true,
+		},
+		{
+			name: "escape left",
+			str:  "test {{company} sample {nonna}",
+			values: map[string]any{
+				"nonna": "888",
+			},
+			expected: "test {{company} sample 888",
+		},
+		{
+			name: "escape right",
+			str:  "test {company}} sample {nonna}",
+			values: map[string]any{
+				"company} sample {nonna": "666",
+			},
+			expected: "test 666",
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			p := Parse(test.str)
+			pr, err := p.Replace(test.values)
+			if !test.expectedErr {
+				assert.NilError(t, err)
+				assert.Equal(t, test.expected, pr)
+			} else {
+				assert.Assert(t, err != nil)
 			}
 		})
 	}
